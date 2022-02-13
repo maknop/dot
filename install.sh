2d461fdb37d02f18ffa7f118c853c37544456b96
Matt Knop <knopmatthew93@gmail.com>
2022-02-13 15:38:12 -0700

    Created shell script for installing dev environment.

---
+++ install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+printf '\n#######################################################\n'
+printf   '##                                                   ##\n'
+printf   '##           Installing Dev Environment              ##\n'
+printf   '##                                                   ##\n'
+printf   '#######################################################'
+
+printf '\n\nChecking if pip is installed.\n'
+if  ! command -v pip &> /dev/null
+then
+    apt install python3-pip
+else
+    printf 'pip is already installed.\n'
+fi
+
+printf '\nChecking if Ansible is installed.\n'
+if ! command -v ansible-playbook &> /dev/null
+then
+    sudo -H pip3 install ansible
+else
+    printf 'Ansible is already installed.\n'
+fi
+
+printf '\n\nCloning ansible-dot git repository into home directory\n'
+cd ~/ && git clone https://github.com/maknop/ansible-dot.git
+
+printf '\n\nInstall ansible-playbook for dev environment.\n'
+cd ~/ansible-dot && ansible-playbook site.yaml
+
+printf '\nRemoved ansible-dot repository from home directory.\n'
+rm -rf ~/ansible-dot
+
+printf '\nHappy developing!!\n\n'
+
+     
