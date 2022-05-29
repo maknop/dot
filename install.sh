dfe45d72f788f6963c95eec06979249ef214b0a6
maknop <knopmatthew@gmail.com>
2022-05-29 00:34:18 -0600

    Updated shell script, added gitignore file.

--- install.sh
+++ install.sh
@@ -1,18 +1,27 @@
-#!/bin/bash
+#!/bin/sh
 DOTFILES_DIR=$(pwd -P "dot")
 echo ${DOTFILES_DIR}
 
-# Install Nix if it does not exist
 if [[ ! -d "$HOME/.nix-profile" ]]; then
     echo "Installing Nix"
     curl -L https://nixos.org/nix/install | sh
 fi
 
-# # Symlink files to home directory
-ln -s ${DOTFILES_DIR}/dotfiles/. $HOME/
+. $HOME/.nix-profile/etc/profile.d/nix.sh
 
-# Install oh-my-zsh if it does not exist
-if [[ ! -d "$HOME/.oh-my-zsh" ]]; then
-    echo "Installing oh-my-zsh"
-    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc
+SYMLINK_LOCATION=$HOME
+if [ ! -e $SYMLINK_LOCATION ]; then
+    echo "Symlinking files to home directory"
+    ln -s ${DOTFILES_DIR}/files/. $HOME/
 fi
+
+OMF_INSTALL_LOCATION=$HOME/.local/share/omf
+if [[ ! -d $OMF_INSTALL_LOCATION ]]; then
+    echo "Install oh-my-fish"
+    curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+fi
+
+echo "Installing oh-my-fish theme(s)"
+fish -c "omf install ays 2> /dev/null"
+
+echo "Finished!"
